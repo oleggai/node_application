09f171e1d5e9de71b1d521f2febfc04c18d88651
ogaydaenko <ogaydaenko@dynamo-ny.com>
2017-03-23 13:47:39 +0200

    npm install, bower install jquery bootstrap. Folder Public right click, mark directory as resources root

--- app.js
+++ app.js
@@ -9,36 +9,35 @@ var config = require('./config');
 var log = require('./lib/log.js')(module);
 
 var app = express();
+
+app.engine('ejs', require('ejs-locals'));
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'ejs');
+
 http.createServer(app).listen(config.get('port'), function(){
     log.info('info','Express server listening on port ' + config.get('port'));
 });
 
-app.use(function(request, response, next) {
-    if(request.url == '/') {
-        response.end('Hello mudila');
-    } else {
-        next();
-    }
+// all environments
+app.use(express.favicon());
+
+if(app.get('env') == 'development') {
+    app.use(express.logger('dev'));
+} else {
+    app.use(express.logger('default'));
+}
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(request, response, next) {
+    // or response.send(200, "Hello user");
+    response.render('index');
 });
 
 app.use(function(request, response, next) {
-    if(request.url == '/test') {
-        response.end('Test');
-    } else {
-        next();
-    }
-});
-app.use(function(request, response, next) {
-    if(request.url == '/forbidden') {
-        next(new Error('Wops. Access, denied'));
-    } else {
-        next();
-    }
-});
-
-app.use(function(request, response, next) {
-    response.send(404, 'Page not found');
-
+    response.send(404, 'Sorry, Page not found');
 });
 
 app.use(function(error, request, response, next) {
@@ -54,19 +53,6 @@ app.use(function(error, request, response, next) {
 var routes = require('./routes');
 var user = require('./routes/user');
 
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.session({ secret: 'your secret here' }));
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
